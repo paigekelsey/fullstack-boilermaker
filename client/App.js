b3ccc9e3fc2cbc887d3be94d3dd6d3176ba19e0d
Kelsey <pages.kelsey@gmail.com>
2021-12-20 12:52:36 -0500

    jwt config

--- client/App.js
+++ client/App.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React from 'react';
 
-import Navbar from "./components/Navbar";
-import Routes from "./Routes";
+import Navbar from './components/Navbar';
+import Routes from './Routes';
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      HELLOWORLD
       <Routes />
     </div>
   );
