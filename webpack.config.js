fc28d1b9bb74cba160cacd0f61edb6c520f5f149
Kelsey <pages.kelsey@gmail.com>
2021-12-20 12:35:36 -0500

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  entry: ["./client/index.js"],
+  entry: ['babel-polyfill', './client/index.js'],
   output: {
     path: __dirname,
-    filename: "./public/bundle.js",
+    filename: './public/bundle.js',
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         options: {
-          presets: ["@babel/preset-react"],
+          presets: ['@babel/preset-react'],
         },
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
